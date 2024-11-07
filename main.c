4104dc0b46d83f2043b92ab497575fb709829f5d
tonyhdz24 <tonyhernandez24@u.boisestate.edu>
2024-11-06 20:23:18 -0700

    echo command refactored

--- main.c
+++ main.c
@@ -20,28 +20,39 @@ int main(int argc, char const *argv[]) {
     };
 
     char *cmd = malloc(50 * sizeof(char));
+    char *arg = malloc(50 * sizeof(char));
 
     int inputStrLength = strlen(inputStr);
+    int c = 0;
 
     // Parse command
-    int c = 0;
     for (c; inputStr[c] != ' '; c++) {
       cmd[c] = inputStr[c];
     }
     cmd[c] = '\0';
+    c++;
+    // Parse Argument
+    int argIndex = 0;
+    for (c; inputStr[c] != '\0'; c++) {
+      arg[argIndex] = inputStr[c];
+      argIndex++;
+    }
+    arg[c] = '\0';
 
     //* Handling Commands
     if (strcmp(cmd, "exit") == 0) {
       printf("***Good-Bye***\n");
       exit(0);
     } else if (strcmp(cmd, "echo") == 0) {
-      for (c; inputStr[c] != '\0'; c++) {
-        printf("%c", inputStr[c]);
-      }
-      printf("\n");
+      printf("%s \n", arg);
+    } else if (strcmp(cmd, "type") == 0) {
+      printf("%s is a shell builtin", cmd);
     } else {
       printf("%s: command not found\n", inputStr);
     }
+    // free allocated memory
+    free(cmd);
+    free(arg);
     free(inputStr);
   }
   return 0;
