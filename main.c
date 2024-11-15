56aa52d484d5830e60f5615855bfd63035d89a91
tonyhdz24 <tonyhernandez24@u.boisestate.edu>
2024-11-14 20:37:35 -0700

    Added strTrim to remove trailing newline character

--- main.c
+++ main.c
@@ -1,24 +1,52 @@
 #include <stdio.h>
-#define LINE_LENGTH  81
+#include <string.h>
+#define LINE_LENGTH 81
 
-int main(int argc, char const *argv[]){
-
-  // Main shell loop
-  miso_loop();
-
-  return 0;
-}
-
+// Removes trailing newline of a string
+void strTrim(char *);
+void strTrim(char *str) {
+  int idx = 0;
+  // Scan string until we reach the end '\0'
+  while (str[idx] != '\0') {
+    idx++;
+  }
+  // Newline char is right before but now become the end
+  str[idx - 1] = '\0';
+};
 
 // Main Shell Loop
+void miso_read(char *line);
+void miso_read(char *line) {
+  fgets(line, 80, stdin);
+  // Removing the trailing newline character
+  strTrim(line);
+};
 // Parts of the Shell loop
-// 1.) Read
-// 2.) Parse
-// 3.) Execute
-void miso_loop(){
-  // This line variable will contain the command input
-  char line[81];
-  miso_read();
-  miso_parse();
-  miso_exe();
-}
+// 1.) Read - Read the command from standard input
+// 2.) Parse - Separate the command string into a program and arguments
+// 3.) Execute - Run the parsed command
+void miso_loop();
+void miso_loop() {
+  // This line variable will contain the entire command input
+  char line[LINE_LENGTH];
+  // cmd will contain just the command
+  char cmd[LINE_LENGTH / 2];
+
+  printf("$ ");
+  // Read the command from standard input
+  miso_read(line);
+  //? DEBUG
+  printf("%s\n", line);
+  // miso_parse();
+  // miso_exe();
+};
+
+int main() {
+  int shellStatus = 1;
+  // Main shell loop
+  while (shellStatus) {
+    miso_loop();
+  }
+
+  return 0;
+};
